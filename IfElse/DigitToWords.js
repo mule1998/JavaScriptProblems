7486ab6ab936fcd9bebe8f8ad03f33aebec62dc2
Shubham Mule <shubhambmule@gmail.com>
2021-11-18 23:10:06 +0530

    Added: Digit to words

---
+++ IfElse/DigitToWords.js
@@ -0,0 +1,36 @@
+var readlineSync = require('readline-sync')
+
+var number = readlineSync.question('Enter Number: ')
+if (number == 0) {
+    console.log("ZERO")
+}
+else if (number == 1) {
+    console.log("ONE")
+}
+else if (number == 2) {
+    console.log("TWO")
+}
+else if (number == 3) {
+    console.log("THREE")
+}
+else if (number == 4) {
+    console.log("FOUR")
+}
+else if (number == 5) {
+    console.log("FIVE")
+}
+else if (number == 6) {
+    console.log("SIX")
+}
+else if (number == 7) {
+    console.log("SEVEN")
+}
+else if (number == 8) {
+    console.log("EIGHT")
+}
+else if (number == 9) {
+    console.log("NINE")
+}
+else {
+    console.log("Only single Digit")
+}
